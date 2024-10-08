82dc6bc40dd0fe8650e92d385bc3cb7beace56cb
Supun <Supuntharakaxz@gamil.com>
2024-10-08 15:55:13 +0530

    Skelton-project

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,26 @@
+function HeroSection() {
+    return (
+        <div className="border-t border-b py-10 flex items-center justify-center">
+            <div className="flex flex-col items-center text-center lg:w-1/4">
+
+                <h1 className="text-6xl tracking-tight pb-5">
+                    Transforming Visions into Digital Realities
+                </h1>
+
+                <div>
+                    <p className="flex flex-col items-center text-center mx-20">
+                        your content | your content | your content | your content | your content | your content | your content | your content | your content | your content |
+                    </p>
+                </div>
+
+                <div className="pt-5">
+                    <button class="bg-black hover:bg-white text-white font-semibold hover:text-black py-2 px-4 border border-white hover:border-black rounded-3xl">
+                        Enter Your Button Name Here
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HeroSection;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,31 @@
+import logo from '../assets/logo.png'
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
+function NavBar() {
+    return (
+        <div className="border-t border-b mb-10 flex justify-between md:text-2xl lg:text-2xl py-6 px-5">
+
+            <div>
+                <ul className="flex justify-between">
+                    <li className="px-2">Home</li>
+                    <li className="px-2">Home</li>
+                    <li className="px-2">Home</li>
+                    <li className="px-2">Home</li>
+                    <li className="px-2">Home</li>
+                </ul>
+            </div>
+
+            <div className="flex flex-shrink-0 items-center">
+                <img className="mx-2 w-12" src={logo} alt="NavBar Logo" />
+            </div>
+
+            <div className='flex justify-center gap-2'>
+                <FaGithub />
+                <FaLinkedin />
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
